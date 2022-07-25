15dd051d1c041a397a166540bdee80f14637650a
kripesh101 <33386091+kripesh101@users.noreply.github.com>
2022-07-25 09:31:53 +0545

    Local dev mode now runs in host mode
    Properly fixed CORS issues

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,28 @@
 from typing import Union
+import os
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "https://thrifty.pages.dev",
-    "https://*.thrifty.pages.dev"
-]
+prod = os.getenv("PROD")
+
+origins = ["http://*"]
+regex = "http://.*"
+
+if (prod != None):
+    origins = [
+        "https://thrifty.pages.dev",
+        "https://*.thrifty.pages.dev"
+    ]
+    regex = "https://.*\.thrifty\.pages\.dev"
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
+    allow_origin_regex=regex,
     allow_methods=["*"],
     allow_headers=["*"],
 )
--- web/src/lib/backend.js
+++ web/src/lib/backend.js
@@ -1,5 +1,11 @@
-const backend = import.meta.env.DEV
-    ? "http://127.0.0.1:8000/"
-    : "https://testsite.southeastasia.cloudapp.azure.com:3000/";
+let backend;
+if (import.meta.env.DEV) {
+    const url = new URL(window.location.href);
+    url.port = "8000";
+    url.pathname = "";
+    backend = url.origin;
+} else {
+    backend = "https://testsite.southeastasia.cloudapp.azure.com:3000/";
+}
 
 export default backend;
