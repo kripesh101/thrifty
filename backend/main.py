1c9ed8e33ccfa5e44acb90067b3f281c1705020f
AKIRAFOX <shakyasarthak2002@gmail.com>
2022-08-04 19:19:54 +0545

    database

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from typing import Union
 import os
+import sqlite3
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -27,10 +28,28 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+import sqlite3
+con = sqlite3.connect('data.db')
+
+cur = con.cursor()
+
+# Create table
+cur.execute('''CREATE TABLE stocks
+               (date text, trans text, symbol text, qty real, price real)''')
+
+# Insert a row of data
+cur.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+
+# Save (commit) the changes
+con.commit()
+
+# We can also close the connection if we are done with it.
+# Just be sure any changes have been committed or they will be lost.
+con.close()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Salong": "Gay boi 101"}
 
 
 @app.get("/items/{item_id}")
