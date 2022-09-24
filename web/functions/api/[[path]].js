a21a0b259a6fa67b6f33d61a7e5edd009acfc0af
kripesh101 <33386091+kripesh101@users.noreply.github.com>
2022-09-24 11:50:16 +0545

    fixed pages function and updated npm packages

--- web/functions/api/[[path]].js
+++ web/functions/api/[[path]].js
@@ -1,3 +1,8 @@
-export async function onRequest({ request, params }) {
-    return fetch(`http://testsite.southeastasia.cloudapp.azure.com/${params.path}`, request);
+export async function onRequest({ request }) {
+    const url = new URL(request.url);
+    url.hostname = "testsite.southeastasia.cloudapp.azure.com";
+    url.protocol = "http:";
+    url.pathname = url.pathname.slice(url.pathname.indexOf("/", 1));
+    url.port = "";
+    return fetch(url.toString(), request);
 }
